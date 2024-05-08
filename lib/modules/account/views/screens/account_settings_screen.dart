965de29ae1a144545ae571173c71571de94ac966
20192726 <20192726@fue.edu.eg>
2024-05-08 17:42:25 +0300

    Delete Account Row

--- lib/modules/account/views/screens/account_settings_screen.dart
+++ lib/modules/account/views/screens/account_settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:roadapp2/modules/account/views/widgets/account_text_field.dart';
+import 'package:roadapp2/modules/account/views/widgets/delete_account_row.dart';
 import 'package:roadapp2/shared/const/app_images.dart';
 import 'package:roadapp2/shared/resources/colors.dart';
 import 'package:roadapp2/shared/widgets/custom_appbar.dart';
@@ -18,27 +19,31 @@ class AccountSettingsScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 32.h,),
-            Center(
-              child: Container(
-                width: 110.w,
-                height: 110.h,
-                decoration:const BoxDecoration(
-                  color: AppColors.emptyImageColor,
-                  shape: BoxShape.circle
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 32.h,),
+              Center(
+                child: Container(
+                  width: 110.w,
+                  height: 110.h,
+                  decoration:const BoxDecoration(
+                    color: AppColors.emptyImageColor,
+                    shape: BoxShape.circle
+                  ),
+                  child: Center(child: SvgPicture.asset(AppImages.emptyImageIcon,)),
                 ),
-                child: Center(child: SvgPicture.asset(AppImages.emptyImageIcon,)),
               ),
-            ),
-            SizedBox(height: 32.h,),
-            const AccountTextField(text: 'الاسم',),
-            const AccountTextField(text: 'رقم الهاتف',),
-            const AccountTextField(text: 'البريد الإلكتروني',),
-            const AccountTextField(text: 'كلمة المرور',),
-          ],
+              SizedBox(height: 32.h,),
+              const AccountTextField(text: 'الاسم',),
+              const AccountTextField(text: 'رقم الهاتف',),
+              const AccountTextField(text: 'البريد الإلكتروني',),
+              const AccountTextField(text: 'كلمة المرور',),
+              const DeleteAccountRow(),
+            ],
+          ),
         ),
       ),
     );
