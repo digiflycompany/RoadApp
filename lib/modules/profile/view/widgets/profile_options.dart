5531a9d2e02c53ad516da64901670f98ef6e9d11
20192726 <20192726@fue.edu.eg>
2024-05-27 17:58:27 +0300

    Vendor Maintenance Report Screen

--- lib/modules/profile/view/widgets/profile_options.dart
+++ lib/modules/profile/view/widgets/profile_options.dart
@@ -5,6 +5,7 @@ import 'package:roadapp/modules/contact_us/views/screens/contact_us_screen.dart'
 import 'package:roadapp/modules/coupons_and_gifts/views/screens/coupons_and_gifts_screen.dart';
 import 'package:roadapp/modules/favorite/views/screens/favorite_screen.dart';
 import 'package:roadapp/modules/fuel_consuming_rate/views/screens/fuel_consuming_rate_screen.dart';
+import 'package:roadapp/modules/maintenance%20_report/views/screens/vendor_maintenance_report_screen.dart';
 import 'package:roadapp/modules/profile/view/widgets/profile_option_item.dart';
 import 'package:roadapp/modules/reserve_appointment/views/screens/reserve_appointment_screen.dart';
 import 'package:roadapp/modules/vehicles/views/vehicles_screen.dart';
@@ -41,7 +42,7 @@ class ProfileOptions extends StatelessWidget {
                     image: AppImages.documentIcon,
                     title: "نماذج وتقارير العمل",
                     voidCallback: () {
-                      AppNavigation.navigate(VehiclesScreen());
+                      AppNavigation.navigate(const VendorMaintenanceReportScreen());
                     }),
                 divider(),
                 ProfileOptionItem(
