c61c501ba88e490038a6145e4ff120a8e33a2393
20192726 <20192726@fue.edu.eg>
2024-06-23 16:31:15 +0300

    Vendor Maintenance Table

--- lib/modules/maintenance _report/views/widgets/vendor_maintenance_table.dart
+++ lib/modules/maintenance _report/views/widgets/vendor_maintenance_table.dart
@@ -1,43 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:roadapp/shared/resources/colors.dart';
 
 class VendorMaintenanceTable extends StatelessWidget {
   const VendorMaintenanceTable({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: DataTable(
-        columns: const [
-          DataColumn(label: Text('JO', style: TextStyle(fontWeight: FontWeight.bold))),
-          DataColumn(label: Text('التاريخ', style: TextStyle(fontWeight: FontWeight.bold))),
-          DataColumn(label: Text('نوع الخدمة', style: TextStyle(fontWeight: FontWeight.bold))),
-          DataColumn(label: Text('نوع المنتج', style: TextStyle(fontWeight: FontWeight.bold))),
-          DataColumn(label: Text('السعر', style: TextStyle(fontWeight: FontWeight.bold))),
-        ],
-        rows: const [
-          DataRow(cells: [
-            DataCell(Text('01')),
-            DataCell(Text('15/01/2023')),
-            DataCell(Text('غيار زيت ماتور')),
-            DataCell(Text('شل 05W40')),
-            DataCell(Text('1200')),
-          ]),
-          DataRow(cells: [
-            DataCell(Text('02')),
-            DataCell(Text('15/03/2023')),
-            DataCell(Text('تربيط عفشة')),
-            DataCell(Text('مسمار عجل')),
-            DataCell(Text('150')),
-          ]),
-          DataRow(cells: [
-            DataCell(Text('03')),
-            DataCell(Text('23/04/2023')),
-            DataCell(Text('تنظيف بوابة')),
-            DataCell(Text('بخاخ تنظيف')),
-            DataCell(Text('75')),
-          ]),
-        ],
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppColors.primaryColor,
+            borderRadius: BorderRadius.circular(10.r),
+          ),
+          child: DataTable(
+            columnSpacing: 13.w,
+            headingRowHeight: 40.h,
+            dataRowHeight: 40.h,
+            columns:  [
+              DataColumn(
+                label: Text('JO', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp)),
+              ),
+              DataColumn(
+                label: Text('التاريخ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp)),
+              ),
+              DataColumn(
+                label: Text('نوع الخدمة', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp)),
+              ),
+              DataColumn(
+                label: Text('نوع المنتج', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp)),
+              ),
+              DataColumn(
+                label: Text('السعر', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp)),
+              ),
+            ],
+            rows: [
+              DataRow(
+                cells: [
+                  DataCell(Text('01', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('15/01/2023', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('غيار زيت ماتور', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('شل 05W40', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('1200', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                ],
+                color: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                      return Colors.white; // Use the color you need
+                    }),
+              ),
+              DataRow(
+                cells: [
+                  DataCell(Text('02', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('15/03/2023', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('تربيط عفشة', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('مسمار عجل', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('150', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                ],
+                color: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                      return Colors.white; // Use the color you need
+                    }),
+              ),
+              DataRow(
+                cells: [
+                  DataCell(Text('03', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('23/04/2023', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('تنظيف بوابة', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('بخاخ تنظيف', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                  DataCell(Text('75', style: TextStyle(fontSize: 12.sp,fontWeight: FontWeight.w500))),
+                ],
+                color: MaterialStateProperty.resolveWith<Color?>(
+                        (Set<MaterialState> states) {
+                      return Colors.white; // Use the color you need
+                    }),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
