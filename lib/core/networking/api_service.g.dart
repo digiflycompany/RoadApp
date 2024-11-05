97b888269ad6265848c4b3f03dbb9752ec1c3a86
20192726 <20192726@fue.edu.eg>
2024-11-05 17:38:35 +0200

    Empty maintenance report data Bug fixed in the maintenance report screen

--- lib/core/networking/api_service.g.dart
+++ lib/core/networking/api_service.g.dart
@@ -340,6 +340,43 @@ class _ApiService implements ApiService {
     return _value;
   }
 
+  @override
+  Future<ReportResponse> getReportsList(
+    String token,
+    String parameterValue,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'vehicleId': parameterValue};
+    final _headers = <String, dynamic>{r'Authorization': token};
+    _headers.removeWhere((k, v) => v == null);
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ReportResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/api/v1/reports',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ReportResponse _value;
+    try {
+      _value = ReportResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
--- lib/features/maintenance _report/data/repo/report_repo.dart
+++ lib/features/maintenance _report/data/repo/report_repo.dart
@@ -2,14 +2,7 @@ import 'package:roadapp/core/helpers/logger.dart';
 import 'package:roadapp/core/networking/api_error_handler.dart';
 import 'package:roadapp/core/networking/api_result.dart';
 import 'package:roadapp/core/networking/api_service.dart';
-import 'package:roadapp/features/auth/data/models/login_request_body.dart';
-import 'package:roadapp/features/auth/data/models/login_response.dart';
-import 'package:roadapp/features/auth/data/models/client_register_request_body.dart';
-import 'package:roadapp/features/auth/data/models/client_register_response.dart';
-import 'package:roadapp/features/auth/data/models/provider_register_request_body.dart';
-import 'package:roadapp/features/auth/data/models/provider_register_response.dart';
 import 'package:roadapp/features/maintenance%20_report/data/models/list_reports_model.dart';
-
 import '../../../../core/helpers/cache_helper/cache_helper.dart';
 import '../../../../core/helpers/cache_helper/cache_vars.dart';
 
