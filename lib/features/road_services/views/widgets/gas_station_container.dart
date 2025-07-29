bea8f1edc89283281d18dbc51b881e9f8621fbb3
NairaAli109 <alinaira290@gmail.com>
2025-07-29 15:17:52 +0300

    over flow error fixed

--- lib/features/road_services/views/widgets/gas_station_container.dart
+++ lib/features/road_services/views/widgets/gas_station_container.dart
@@ -35,167 +35,177 @@ class GasStationContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 14.h),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              height: 90.h,
-              decoration: BoxDecoration(
-                  color: AppColors.greyColor3,
-                  borderRadius: BorderRadius.circular(8.r),
-                  border:
-                      Border.all(color: AppColors.primaryColor, width: 1.w)),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 4.w),
-                child: GestureDetector(
-                  onTap: () async {
-                    AppNavigation.navigate(
-                       ServiceMenu(
-                         roadService: roadService,
-
-                      ),
-                    );
-
-                    // showDialog(
-                    //   context: context,
-                    //   builder: (context) {
-                    //     return AlertDialog(
-                    //       scrollable: true,
-                    //       title: Center(
-                    //         child: Text(
-                    //           StringManager.servicesMenu.tr(context),
-                    //           style: TextStyle(
-                    //               fontSize: 14.sp, fontWeight: FontWeight.w600),
-                    //         ),
-                    //       ),
-                    //       content: SizedBox(
-                    //         height: 100.h,
-                    //         child: ListView.builder(
-                    //           itemCount: roadService.services.length,
-                    //             itemBuilder: (context, index) {
-                    //               return Text(
-                    //                 roadService.services[index],
-                    //                 style: TextStyle(
-                    //                     fontSize: 12.sp,
-                    //                     color: AppColors.black,
-                    //                     fontWeight: FontWeight.w600),
-                    //               );
-                    //             },
-                    //         ),
-                    //       ),
-                    //       actions: [
-                    //         TextButton(
-                    //           child: Text(
-                    //             StringManager.cancel.tr(context),
-                    //             style: TextStyle(
-                    //                 fontSize: 12.sp,
-                    //                 color: AppColors.black,
-                    //                 fontWeight: FontWeight.w600),
-                    //           ),
-                    //           onPressed: () => Navigator.pop(context),
-                    //         ),
-                    //       ],
-                    //       //  content: Text(roadService.description),
-                    //     );
-                    //   },
-                    // );
-                  },
-                  child: Column(
-                    children: [
-                      const Spacer(),
-                      Text(
-                        StringManager.servicesMenu.tr(context),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 9.sp,
-                          fontWeight: FontWeight.w600,
+    return SizedBox(
+      height: 150,
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 14.h),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                // height: 90.h,
+                decoration: BoxDecoration(
+                    color: AppColors.greyColor3,
+                    borderRadius: BorderRadius.circular(8.r),
+                    border:
+                        Border.all(color: AppColors.primaryColor, width: 1.w)),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 4.w),
+                  child: GestureDetector(
+                    onTap: () async {
+                      AppNavigation.navigate(
+                        ServiceMenu(
+                          roadService: roadService,
                         ),
-                      ),
-                      const Spacer(),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(width: 3.w),
-          Expanded(
-            flex: 5,
-            child: Container(
-              width: 280.w,
-              height: 90.h,
-              decoration: BoxDecoration(
-                  color: AppColors.greyColor3,
-                  borderRadius: BorderRadius.circular(8.r),
-                  border:
-                      Border.all(color: AppColors.primaryColor, width: 1.w)),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 22.w, vertical: 9.h),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.network(
-                      image,
-                      height: 55.h,
-                      width: 30.w,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return const Icon(
-                          Icons.error,
-                          color: AppColors.red,
-                        );
-                      },
-                    ),
-                    SizedBox(width: 10.w),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      );
+
+                      // showDialog(
+                      //   context: context,
+                      //   builder: (context) {
+                      //     return AlertDialog(
+                      //       scrollable: true,
+                      //       title: Center(
+                      //         child: Text(
+                      //           StringManager.servicesMenu.tr(context),
+                      //           style: TextStyle(
+                      //               fontSize: 14.sp, fontWeight: FontWeight.w600),
+                      //         ),
+                      //       ),
+                      //       content: SizedBox(
+                      //         height: 100.h,
+                      //         child: ListView.builder(
+                      //           itemCount: roadService.services.length,
+                      //             itemBuilder: (context, index) {
+                      //               return Text(
+                      //                 roadService.services[index],
+                      //                 style: TextStyle(
+                      //                     fontSize: 12.sp,
+                      //                     color: AppColors.black,
+                      //                     fontWeight: FontWeight.w600),
+                      //               );
+                      //             },
+                      //         ),
+                      //       ),
+                      //       actions: [
+                      //         TextButton(
+                      //           child: Text(
+                      //             StringManager.cancel.tr(context),
+                      //             style: TextStyle(
+                      //                 fontSize: 12.sp,
+                      //                 color: AppColors.black,
+                      //                 fontWeight: FontWeight.w600),
+                      //           ),
+                      //           onPressed: () => Navigator.pop(context),
+                      //         ),
+                      //       ],
+                      //       //  content: Text(roadService.description),
+                      //     );
+                      //   },
+                      // );
+                    },
+                    child: Column(
                       children: [
-                        Text(title,
-                            style: TextStyle(
-                                fontSize: 12.sp, fontWeight: FontWeight.w600)),
-                        Text(location,
-                            style: TextStyle(
-                                fontSize: 9.sp, fontWeight: FontWeight.w600)),
-                        Text(phone,
-                            style: TextStyle(
-                                fontSize: 11.sp, fontWeight: FontWeight.w600))
+                        const Spacer(),
+                        Text(
+                          StringManager.servicesMenu.tr(context),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 9.sp,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const Spacer(),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(width: 3.w),
-          Expanded(
-            child: Container(
-              height: 90.h,
-              decoration: BoxDecoration(
-                  color: AppColors.greyColor3,
-                  borderRadius: BorderRadius.circular(8.r),
-                  border:
-                      Border.all(color: AppColors.primaryColor, width: 1.w)),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 4.w),
-                child: GestureDetector(
-                  onTap: () async {
-                    Uri url = Uri.parse(roadService.mapsLink);
-                    launchUrl(url);
-                  },
-                  child: Column(
+            SizedBox(width: 3.w),
+            Expanded(
+              flex: 5,
+              child: Container(
+                // height: 90.h,
+                decoration: BoxDecoration(
+                    color: AppColors.greyColor3,
+                    borderRadius: BorderRadius.circular(8.r),
+                    border:
+                        Border.all(color: AppColors.primaryColor, width: 1.w)),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 22.w, vertical: 9.h),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      const Spacer(),
-                      SvgPicture.asset(AppAssets.locationIcon, width: 20.w),
-                      const Spacer(),
+                      Center(
+                        child: Image.network(
+                          image,
+                          height: 55.h,
+                          width: 30.w,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return const Icon(
+                              Icons.error,
+                              color: AppColors.red,
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 10.w),
+                      Expanded(
+                          child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(title,
+                              maxLines: 2,
+                              style: TextStyle(
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w600,
+                                  overflow: TextOverflow.ellipsis)),
+                          Text(location,
+                              style: TextStyle(
+                                  fontSize: 9.sp, fontWeight: FontWeight.w600)),
+                          Text(phone,
+                              style: TextStyle(
+                                  fontSize: 11.sp, fontWeight: FontWeight.w600))
+                        ],
+                      )),
                     ],
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+            SizedBox(width: 3.w),
+            Expanded(
+              child: Container(
+                // height: 90.h,
+                decoration: BoxDecoration(
+                    color: AppColors.greyColor3,
+                    borderRadius: BorderRadius.circular(8.r),
+                    border:
+                        Border.all(color: AppColors.primaryColor, width: 1.w)),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 4.w),
+                  child: GestureDetector(
+                    onTap: () async {
+                      Uri url = Uri.parse(roadService.mapsLink);
+                      launchUrl(url);
+                    },
+                    child: Column(
+                      children: [
+                        const Spacer(),
+                        SvgPicture.asset(AppAssets.locationIcon, width: 20.w),
+                        const Spacer(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
