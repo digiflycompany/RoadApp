278797f9f512d7db52e978f8049fcbd508112463
yousef <yousef.hanafy@gmail.com>
2025-10-03 19:34:22 +0300

    Changed to production

--- lib/core/networking/api_service.g.dart
+++ lib/core/networking/api_service.g.dart
@@ -10,7 +10,7 @@ part of 'api_service.dart';
 
 class _ApiService implements ApiService {
   _ApiService(this._dio, {this.baseUrl, this.errorLogger}) {
-    baseUrl ??= 'https://roadapp-api.vercel.app/';
+    baseUrl ??= 'https://api.roadapp.net/';
   }
 
   final Dio _dio;
