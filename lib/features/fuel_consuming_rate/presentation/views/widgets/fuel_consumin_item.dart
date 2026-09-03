d0542eb20244dd47f4dcc291691a278260908779
osama <o.omrann@storeus.com>
2026-09-03 23:31:18 +0300

    Delete Rides ✔

--- lib/features/fuel_consuming_rate/presentation/views/widgets/fuel_consumin_item.dart
+++ lib/features/fuel_consuming_rate/presentation/views/widgets/fuel_consumin_item.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:roadapp/core/Theming/styles.dart';
+import 'package:roadapp/core/helpers/functions/toast.dart';
 import 'package:roadapp/core/helpers/localization/app_localization.dart';
 import 'package:roadapp/core/helpers/string_manager.dart';
 import 'package:roadapp/core/widgets/components.dart';
@@ -23,7 +24,8 @@ class FuelConsumingItem extends StatelessWidget {
 
   final Ride rate;
 
-  void _showDeleteConfirmationDialog(BuildContext context, FuelConsumingRateCubit cubit) {
+  void _showDeleteConfirmationDialog(
+      BuildContext context, FuelConsumingRateCubit cubit) {
     showDialog(
       context: context,
       builder: (dialogContext) => AlertDialog(
@@ -85,92 +87,90 @@ class FuelConsumingItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<FuelConsumingRateCubit, FuelConsumingRateStates>(
-        listener: (_, state) {},
-        builder: (_, state) {final FuelConsumingRateCubit cubit = FuelConsumingRateCubit.get(context);
-          return Container(
-              decoration: BoxDecoration(
-                  color: AppColors.whiteColor2,
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all()),
-              child: Column(children: [
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(children: [
-                      SvgPicture.asset(AppAssets.fuelCounter,
-                          height: 17, width: 17),
-                      const SizedBox(width: 5),
-                      Text(
-                          '${StringManager.fuelMeterBeforeAddingFuel.tr(context)}: ${rate.odometerBefore} ${StringManager.km.tr(context)}',
-                          style: const TextStyle(fontSize: 8)),
-                      const Gap(12),
-                      SvgPicture.asset(AppAssets.kmBoard,
-                          height: 17, width: 17),
-                      const SizedBox(width: 5),
-                      Text(
-                          '${StringManager.kms.tr(context)}: ${rate.kmCount} ${StringManager.km.tr(context)}',
-                          style: const TextStyle(fontSize: 8))
-                    ])),
-                divider(),
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(children: [
-                      SvgPicture.asset(AppAssets.kmBoard,
-                          height: 17, width: 17),
-                      const SizedBox(width: 5),
-                      Text(
-                          '${StringManager.kmGm.tr(context)}: ${rate.kmPerEGP} ${StringManager.kmGmII.tr(context)}',
-                          style: const TextStyle(fontSize: 8)),
-                      const Gap(12),
-                      SvgPicture.asset(AppAssets.kmBoard,
-                          height: 17, width: 17),
-                      const SizedBox(width: 5),
-                      Text(
-                          '${StringManager.kmLiter.tr(context)}: ${rate.kmPerLiter} ${StringManager.kmLiterII.tr(context)}',
-                          style: const TextStyle(fontSize: 8))
-                    ])),
-                divider(),
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(children: [
-                      SvgPicture.asset(AppAssets.fuelGun,
-                          height: 17, width: 17),
-                      const SizedBox(width: 5),
-                      Expanded(
-                          child: Text(
-                              '${StringManager.liters.tr(context)}: ${rate.literCount} ${StringManager.l.tr(context)}',
-                              style: const TextStyle(fontSize: 8),
-                              overflow: TextOverflow.ellipsis)),
-                      const Gap(12),
-                      SvgPicture.asset(AppAssets.moneyIcon,
-                          height: 17, width: 17),
-                      const SizedBox(width: 5),
-                      Text(
-                          '${StringManager.literPrice.tr(context)}: ${rate.literPrice} ${StringManager.le.tr(context)}',
-                          style: const TextStyle(fontSize: 8)),
-                      const Gap(12),
-                      SvgPicture.asset(AppAssets.moneyIcon,
-                          height: 17, width: 17),
-                      const SizedBox(width: 5),
-                      Text(
-                          "${StringManager.fullTankPrice.tr(context)}: ${rate.fullTankPrice} ${StringManager.le.tr(context)}",
-                          style: const TextStyle(fontSize: 8))
-                    ])),
-                Gap(12.h),
-                Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  CustomElevatedButton(
-                      onTap: () => _showDeleteConfirmationDialog(context, cubit),
-                      widget: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Icon(CupertinoIcons.delete, size: 14.sp),
-                            Text(StringManager.delete.tr(context),
-                                style: Styles.textStyle12)
-                          ]),
-                      color: Colors.redAccent.withValues(alpha: .25)),
-                  Gap(10.w)
-                ]),
-                Gap(10.h)
-              ]));
-        });
+        listener: (_, state) {
+      if (state is DeleteSingleRideErrorState) {
+        showToast(message: state.errorMessage, state: ToastStates.error);
+      }
+    }, builder: (_, state) {
+      final FuelConsumingRateCubit cubit = FuelConsumingRateCubit.get(context);
+      return Container(
+          decoration: BoxDecoration(
+              color: AppColors.whiteColor2,
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all()),
+          child: Column(children: [
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: [
+                  SvgPicture.asset(AppAssets.fuelCounter,
+                      height: 17, width: 17),
+                  const SizedBox(width: 5),
+                  Text(
+                      '${StringManager.fuelMeterBeforeAddingFuel.tr(context)}: ${rate.odometerBefore} ${StringManager.km.tr(context)}',
+                      style: const TextStyle(fontSize: 8)),
+                  const Gap(12),
+                  SvgPicture.asset(AppAssets.kmBoard, height: 17, width: 17),
+                  const SizedBox(width: 5),
+                  Text(
+                      '${StringManager.kms.tr(context)}: ${rate.kmCount} ${StringManager.km.tr(context)}',
+                      style: const TextStyle(fontSize: 8))
+                ])),
+            divider(),
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: [
+                  SvgPicture.asset(AppAssets.kmBoard, height: 17, width: 17),
+                  const SizedBox(width: 5),
+                  Text(
+                      '${StringManager.kmGm.tr(context)}: ${rate.kmPerEGP} ${StringManager.kmGmII.tr(context)}',
+                      style: const TextStyle(fontSize: 8)),
+                  const Gap(12),
+                  SvgPicture.asset(AppAssets.kmBoard, height: 17, width: 17),
+                  const SizedBox(width: 5),
+                  Text(
+                      '${StringManager.kmLiter.tr(context)}: ${rate.kmPerLiter} ${StringManager.kmLiterII.tr(context)}',
+                      style: const TextStyle(fontSize: 8))
+                ])),
+            divider(),
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(children: [
+                  SvgPicture.asset(AppAssets.fuelGun, height: 17, width: 17),
+                  const SizedBox(width: 5),
+                  Expanded(
+                      child: Text(
+                          '${StringManager.liters.tr(context)}: ${rate.literCount} ${StringManager.l.tr(context)}',
+                          style: const TextStyle(fontSize: 8),
+                          overflow: TextOverflow.ellipsis)),
+                  const Gap(12),
+                  SvgPicture.asset(AppAssets.moneyIcon, height: 17, width: 17),
+                  const SizedBox(width: 5),
+                  Text(
+                      '${StringManager.literPrice.tr(context)}: ${rate.literPrice} ${StringManager.le.tr(context)}',
+                      style: const TextStyle(fontSize: 8)),
+                  const Gap(12),
+                  SvgPicture.asset(AppAssets.moneyIcon, height: 17, width: 17),
+                  const SizedBox(width: 5),
+                  Text(
+                      "${StringManager.fullTankPrice.tr(context)}: ${rate.fullTankPrice} ${StringManager.le.tr(context)}",
+                      style: const TextStyle(fontSize: 8))
+                ])),
+            Gap(12.h),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              CustomElevatedButton(
+                  onTap: () => _showDeleteConfirmationDialog(context, cubit),
+                  widget: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(CupertinoIcons.delete, size: 14.sp),
+                        Text(StringManager.delete.tr(context),
+                            style: Styles.textStyle12)
+                      ]),
+                  color: Colors.redAccent.withValues(alpha: .25)),
+              Gap(10.w)
+            ]),
+            Gap(10.h)
+          ]));
+    });
   }
 }
